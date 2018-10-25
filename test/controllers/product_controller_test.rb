de81f98397be1ec8c3eacdbd00cf5145d6595926
juanraba <jrabanillog@gmail.com>
2018-10-25 20:39:31 -0300

    redirect test, adjust to wanted

--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ProductControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   # test "the truth" do
   #   assert true
   # end
+  test 'user not logged will be redirect' do
+    get root_path
+    assert_response :redirect
+  end
+
+  test 'user logged in wont be redirect' do
+    user = users(:one)
+    sign_in(user)
+    get root_path
+    assert_response :success
+  end
 end
